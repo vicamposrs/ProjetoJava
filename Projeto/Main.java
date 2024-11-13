ec4adf6ff1a956bdc2591a2ec9392b91e698aea8
vicamposrs <vitorbao7@gmail.com>
2024-11-12 23:49:16 -0300

    Update Main.java

--- Projeto/Main.java
+++ Projeto/Main.java
@@ -1,12 +1,6 @@
-package application;
 
 import java.util.Date;
 import java.util.Locale;
-
-import entities.Cliente;
-import entities.Conta;
-import entities.ContaCorrente;
-import entities.ContaPoupanca;
 
 public class Main {
     public static void main(String[] args) {
