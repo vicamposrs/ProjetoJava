7fc2780e036d4a1e56e05f6c3f17fb474318d2ce
vicamposrs <vitorbao7@gmail.com>
2024-11-22 18:26:21 -0300

    Update Conta.java

--- Projeto/Conta.java
+++ Projeto/Conta.java
@@ -1,24 +1,36 @@
 import java.util.ArrayList;
 import java.util.List;
 
+
 public abstract class Conta {
     // Atributos
     protected Cliente titular; // Criar uma classe cliente que será o Titular
+    private String numeroConta;
     private double saldo;
+    private String senha;
     protected List<Transacao> extrato;
     
     
 
     // Construtor
-    public Conta(Cliente titular, double saldoInicial) {
+    public Conta(Cliente titular,String numeroConta, double saldoInicial,String senha) {
         this.titular = titular;
+        this.numeroConta = numeroConta;
         this.saldo = saldoInicial;
+        this.senha = senha;
         this.extrato = new ArrayList<>();
+        
+        if(saldo > 0) extrato.add(new Transacao(TipoTransacao.DEPOSITO,saldoInicial,this));
     }
 
     // Getters e Setters 
     public Cliente getTitular() {
         return titular;
+    }
+    
+    public String getNumeroConta()
+    {
+    	return numeroConta;
     }
 
     public double getSaldo() {
@@ -32,10 +44,12 @@ public abstract class Conta {
     // Métodos
     public abstract void sacar(double valor);
     public abstract void depositar(double valor);
+    
+    	
+  
 
-    public void exibirInformacoes() {
-        System.out.println("Titular: " + titular.getNome());
-        System.out.println("Saldo: " + saldo);
+    public String exibirInformacoes() {
+        return "nº:" + numeroConta + " Cliente: " + titular.getNome() + " Saldo: R$%.2f".formatted(saldo);
     }
     
     public void exibirExtrato()
@@ -47,4 +61,22 @@ public abstract class Conta {
     	}
     	System.out.printf("Saldo: %.2f",saldo);
     }
+
+	public boolean verificarSenha(String senha) {
+		if(this.senha.equals(senha)) return true;
+		else return false;
+	}
+
+	public void transferir(Double valor, Conta contaDestino) {
+		if (getSaldo() >= valor) {
+            setSaldo(getSaldo() - valor);
+            contaDestino.setSaldo(contaDestino.getSaldo()+valor);
+            InterfaceATM.exibirMensagem("Transferencia de R$ " + valor + " realizado com sucesso!\n");
+            extrato.add(new Transacao(TipoTransacao.TRANSFERENCIA_ORIGEM,valor,this,contaDestino));
+            contaDestino.extrato.add(new Transacao(TipoTransacao.TRANSFERENCIA_DESTINO,valor,this,contaDestino));
+        } else {
+            System.out.println("Saldo insuficiente para saque.");
+        }
+		
+	}
 }
